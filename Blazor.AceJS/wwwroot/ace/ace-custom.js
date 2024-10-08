542528ecd49c7385303a3dd4a25e05eb8e923341
Benoit <benoit.zins@outlook.fr>
2024-10-08 16:15:55 +0200

    v.1.1.5 improved performances

--- Blazor.AceJS/wwwroot/ace/ace-custom.js
+++ Blazor.AceJS/wwwroot/ace/ace-custom.js
@@ -1,56 +1,65 @@
+(function () {
+    let previousCode = null;
 
-window.editorRender = function (element, mode, theme, readOnly){
-    var editor = ace.edit(element);
-    var dragging = false;
-    var wpoffset = 0;
-    var defineMode = "ace/mode/" + mode;
+    function getEditor(element) {
+        return ace.edit(element);
+    }
 
-    editor.setTheme("ace/theme/" + theme);
-    editor.setReadOnly(readOnly);
+    function setEditorMode(editor, mode) {
+        var defineMode = "ace/mode/" + mode;
+        editor.session.setMode(defineMode);
+    }
 
-    editor.session.setMode(defineMode);
-    editor.renderer.setScrollMargin(10, 10);
-    // enable autocompletion and snippets
-    editor.setOptions({
-        autoScrollEditorIntoView: true,
-        enableBasicAutocompletion: true,
-        enableSnippets: true,
-        enableLiveAutocompletion: true
-    });
+    window.editorRender = function (element, mode, theme, readOnly) {
+        var editor = getEditor(element);
 
-};
+        editor.setTheme("ace/theme/" + theme);
+        editor.setReadOnly(readOnly);
+        setEditorMode(editor, mode);
+        editor.renderer.setScrollMargin(10, 10);
 
-window.ace_set_theme = function (element, theme) {
-    var editor = ace.edit(element);
-    editor.setTheme("ace/theme/" + theme);
-}
+        // enable autocompletion and snippets
+        editor.setOptions({
+            autoScrollEditorIntoView: true,
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: true
+        });
+    };
 
-window.ace_set_mode = function (element, mode) {
-    var editor = ace.edit(element);
-    var defineMode = "ace/mode/" + mode;
-    editor.session.setMode(defineMode);
-}
+    window.ace_set_theme = function (element, theme) {
+        var editor = getEditor(element);
+        editor.setTheme("ace/theme/" + theme);
+    };
 
-window.ace_destroy = function ace_destroy (element) {
-    var editor = ace.edit(element);
-    editor.destroy();
-    editor.container.remove();
-}
+    window.ace_set_mode = function (element, mode) {
+        var editor = getEditor(element);
+        setEditorMode(editor, mode);
+    };
 
-window.ace_set_readonly = function (element, readOnly) {
-    var editor = ace.edit(element);
-    editor.setReadOnly(readOnly);
-}
+    window.ace_destroy = function (element) {
+        var editor = getEditor(element);
+        editor.destroy();
+        editor.container.remove();
+    };
 
-window.GetCode = (dotNetHelper, element) => {
-    var editor = ace.edit(element);
-    var code = editor.getSession().getValue();
-    dotNetHelper.invokeMethodAsync( 'ReceiveCode', code)
-};
+    window.ace_set_readonly = function (element, readOnly) {
+        var editor = getEditor(element);
+        editor.setReadOnly(readOnly);
+    };
 
+    window.GetCode = (dotNetHelper, element) => {
+        var editor = getEditor(element);
+        var code = editor.getSession().getValue();
+        dotNetHelper.invokeMethodAsync('ReceiveCode', code);
+    };
 
-window.GetSelectedCode = (dotNetHelper, element) => {
-    var editor = ace.edit(element);
-    var code = editor.getSelectedText();
-    dotNetHelper.invokeMethodAsync('ReceiveSelectedCode', code)
-};
+    window.GetSelectedCode = (dotNetHelper, element) => {
+        var editor = getEditor(element);
+        var code = editor.getSelectedText();
+        if (code !== previousCode) {
+            previousCode = code;
+            dotNetHelper.invokeMethodAsync('ReceiveSelectedCode', code);
+        }
+    };
+})();
