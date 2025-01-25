17a3a5c92f26868eaf5d6329d0743a9af27f01d6
Benoit <benoit.zins@outlook.fr>
2025-01-26 00:05:06 +0100

    Version 1.2.0 Dotnet8

--- Blazor.AceJS/AceLanguagesList.cs
+++ Blazor.AceJS/AceLanguagesList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,178 +9,351 @@ namespace Blazor.AceEditorJs
 {
     public enum AceLanguage
     {
+        [Display(Name = "ABAP")]
         abap,
+        [Display(Name = "ABC")]
         abc,
+        [Display(Name = "ActionScript")]
         actionscript,
+        [Display(Name = "ADA")]
         ada,
+        [Display(Name = "Alda")]
         alda,
+        [Display(Name = "Apache Conf")]
         apache_conf,
+        [Display(Name = "Apex")]
         apex,
+        [Display(Name = "AQL")]
         aql,
+        [Display(Name = "AsciiDoc")]
         asciidoc,
+        [Display(Name = "ASL")]
         asl,
+        [Display(Name = "Assembly x86")]
         assembly_x86,
+        [Display(Name = "AutoHotkey / AutoIt")]
         autohotkey,
+        [Display(Name = "BatchFile")]
         batchfile,
+        [Display(Name = "C and C++")]
         c_cpp,
+        [Display(Name = "C9Search")]
         c9search,
+        [Display(Name = "Cirru")]
         cirru,
+        [Display(Name = "Clojure")]
         clojure,
+        [Display(Name = "Cobol")]
         cobol,
+        [Display(Name = "CoffeeScript")]
         coffee,
+        [Display(Name = "ColdFusion")]
         coldfusion,
+        [Display(Name = "Crystal")]
         crystal,
+        [Display(Name = "C#")]
         csharp,
+        [Display(Name = "Csound Document")]
         csound_document,
+        [Display(Name = "Csound")]
         csound_orchestra,
+        [Display(Name = "Csound Score")]
         csound_score,
+        [Display(Name = "CSS")]
         css,
+        [Display(Name = "Curly")]
         curly,
+        [Display(Name = "D")]
         d,
+        [Display(Name = "Dart")]
         dart,
+        [Display(Name = "Diff")]
         diff,
+        [Display(Name = "Dockerfile")]
         dockerfile,
+        [Display(Name = "Dot")]
         dot,
+        [Display(Name = "Drools")]
         drools,
+        [Display(Name = "Edifact")]
         edifact,
+        [Display(Name = "Eiffel")]
         eiffel,
+        [Display(Name = "EJS")]
         ejs,
+        [Display(Name = "Elixir")]
         elixir,
+        [Display(Name = "Elm")]
         elm,
+        [Display(Name = "Erlang")]
         erlang,
+        [Display(Name = "Forth")]
         forth,
+        [Display(Name = "Fortran")]
         fortran,
+        [Display(Name = "FSharp")]
         fsharp,
+        [Display(Name = "FSL")]
         fsl,
+        [Display(Name = "FreeMarker")]
         ftl,
+        [Display(Name = "Gcode")]
         gcode,
+        [Display(Name = "Gherkin")]
         gherkin,
+        [Display(Name = "Gitignore")]
         gitignore,
+        [Display(Name = "Glsl")]
         glsl,
+        [Display(Name = "Gobstones")]
         gobstones,
+        [Display(Name = "Go")]
         golang,
+        [Display(Name = "GraphQLSchema")]
         graphqlschema,
+        [Display(Name = "Groovy")]
         groovy,
+        [Display(Name = "HAML")]
         haml,
+        [Display(Name = "Handlebars")]
         handlebars,
+        [Display(Name = "Haskell")]
         haskell,
+        [Display(Name = "Haskell Cabal")]
         haskell_cabal,
+        [Display(Name = "haXe")]
         haxe,
+        [Display(Name = "Hjson")]
         hjson,
+        [Display(Name = "HTML")]
         html,
+        [Display(Name = "HTML (Elixir)")]
         html_elixir,
+        [Display(Name = "HTML (Ruby)")]
         html_ruby,
+        [Display(Name = "INI")]
         ini,
+        [Display(Name = "Io")]
         io,
+        [Display(Name = "Ion")]
         ion,
+        [Display(Name = "Jack")]
         jack,
+        [Display(Name = "Jade")]
         jade,
+        [Display(Name = "Java")]
         java,
+        [Display(Name = "JavaScript")]
         javascript,
+        [Display(Name = "JSON")]
         json,
+        [Display(Name = "JSON5")]
         json5,
+        [Display(Name = "JSONiq")]
         jsoniq,
+        [Display(Name = "JSP")]
         jsp,
+        [Display(Name = "JSSM")]
         jssm,
+        [Display(Name = "JSX")]
         jsx,
+        [Display(Name = "Julia")]
         julia,
+        [Display(Name = "Kotlin")]
         kotlin,
+        [Display(Name = "LaTeX")]
         latex,
+        [Display(Name = "Latte")]
         latte,
+        [Display(Name = "LESS")]
         less,
+        [Display(Name = "Liquid")]
         liquid,
+        [Display(Name = "Lisp")]
         lisp,
+        [Display(Name = "LiveScript")]
         livescript,
+        [Display(Name = "Log")]
         log,
+        [Display(Name = "LogiQL")]
         logiql,
+        [Display(Name = "LSL")]
         lsl,
+        [Display(Name = "Lua")]
         lua,
+        [Display(Name = "LuaPage")]
         luapage,
+        [Display(Name = "Lucene")]
         lucene,
+        [Display(Name = "Makefile")]
         makefile,
+        [Display(Name = "Markdown")]
         markdown,
+        [Display(Name = "Mask")]
         mask,
+        [Display(Name = "MATLAB")]
         matlab,
+        [Display(Name = "Maze")]
         maze,
+        [Display(Name = "MediaWiki")]
         mediawiki,
+        [Display(Name = "MEL")]
         mel,
+        [Display(Name = "MIPS")]
         mips,
+        [Display(Name = "MIXAL")]
         mixal,
+        [Display(Name = "MUSHCode")]
         mushcode,
+        [Display(Name = "MySQL")]
         mysql,
+        [Display(Name = "Nginx")]
         nginx,
+        [Display(Name = "Nim")]
         nim,
+        [Display(Name = "Nix")]
         nix,
+        [Display(Name = "NSIS")]
         nsis,
+        [Display(Name = "Nunjucks")]
         nunjucks,
+        [Display(Name = "Objective-C")]
         objectivec,
+        [Display(Name = "OCaml")]
         ocaml,
+        [Display(Name = "PartiQL")]
         partiql,
+        [Display(Name = "Pascal")]
         pascal,
+        [Display(Name = "Perl")]
         perl,
+        [Display(Name = "pgSQL")]
         pgsql,
+        [Display(Name = "PHP (Blade Template)")]
         php_laravel_blade,
+        [Display(Name = "PHP")]
         php,
+        [Display(Name = "Pig")]
         pig,
+        [Display(Name = "Powershell")]
         powershell,
+        [Display(Name = "Praat")]
         praat,
+        [Display(Name = "Prisma")]
         prisma,
+        [Display(Name = "Prolog")]
         prolog,
+        [Display(Name = "Properties")]
         properties,
+        [Display(Name = "Protobuf")]
         protobuf,
+        [Display(Name = "Puppet")]
         puppet,
+        [Display(Name = "Python")]
         python,
+        [Display(Name = "QML")]
         qml,
+        [Display(Name = "R")]
         r,
+        [Display(Name = "Raku")]
         raku,
+        [Display(Name = "Razor")]
         razor,
+        [Display(Name = "RDoc")]
         rdoc,
+        [Display(Name = "Red")]
         red,
+        [Display(Name = "RHTML")]
         rhtml,
+        [Display(Name = "Robot")]
         robot,
+        [Display(Name = "RST")]
         rst,
+        [Display(Name = "Ruby")]
         ruby,
+        [Display(Name = "Rust")]
         rust,
+        [Display(Name = "SaC")]
         sac,
+        [Display(Name = "SASS")]
         sass,
+        [Display(Name = "SCAD")]
         scad,
+        [Display(Name = "Scala")]
         scala,
+        [Display(Name = "Scheme")]
         scheme,
+        [Display(Name = "Scrypt")]
         scrypt,
+        [Display(Name = "SCSS")]
         scss,
+        [Display(Name = "SH")]
         sh,
+        [Display(Name = "SJS")]
         sjs,
+        [Display(Name = "Slim")]
         slim,
+        [Display(Name = "Smarty")]
         smarty,
+        [Display(Name = "Smithy")]
         smithy,
+        [Display(Name = "snippets")]
         snippets,
+        [Display(Name = "Soy Template")]
         soy_template,
+        [Display(Name = "Space")]
         space,
+        [Display(Name = "SQL")]
         sql,
+        [Display(Name = "SQLServer")]
         sqlserver,
+        [Display(Name = "Stylus")]
         stylus,
+        [Display(Name = "SVG")]
         svg,
+        [Display(Name = "Swift")]
         swift,
+        [Display(Name = "Tcl")]
         tcl,
+        [Display(Name = "Terraform")]
         terraform,
+        [Display(Name = "Tex")]
         tex,
+        [Display(Name = "Text")]
         text,
+        [Display(Name = "Textile")]
         textile,
+        [Display(Name = "Toml")]
         toml,
+        [Display(Name = "TSX")]
         tsx,
+        [Display(Name = "Twig")]
         twig,
+        [Display(Name = "Typescript")]
         typescript,
+        [Display(Name = "Vala")]
         vala,
+        [Display(Name = "VBScript")]
         vbscript,
+        [Display(Name = "Velocity")]
         velocity,
+        [Display(Name = "Verilog")]
         verilog,
+        [Display(Name = "VHDL")]
         vhdl,
+        [Display(Name = "Visualforce")]
         visualforce,
+        [Display(Name = "Wollok")]
         wollok,
+        [Display(Name = "XML")]
         xml,
+        [Display(Name = "XQuery")]
         xquery,
+        [Display(Name = "YAML")]
         yaml,
+        [Display(Name = "Zeek")]
         zeek,
+        [Display(Name = "Django")]
         django
     }
 }
--- Blazor.AceJS/AceThemesList.cs
+++ Blazor.AceJS/AceThemesList.cs
@@ -1,50 +1,156 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blazor.AceEditorJs
 {
+
+    public enum AceThemeType
+    {
+        Bright,
+        Dark
+    }
+
     public enum AceTheme
     {
+        [Display(Name = "Chrome")]
+        chrome,
+        [Display(Name = "Clouds")]
         clouds,
+        [Display(Name = "Crimson Editor")]
         crimson_editor,
+        [Display(Name = "Dawn")]
         dawn,
+        [Display(Name = "Dreamweaver")]
         dreamweaver,
+        [Display(Name = "Eclipse")]
         eclipse,
+        [Display(Name = "GitHub")]
         github,
+        [Display(Name = "iPlastic")]
         iplastic,
+        [Display(Name = "Solarized Light")]
         solarized_light,
+        [Display(Name = "TextMate")]
         textmate,
+        [Display(Name = "Tomorrow")]
         tomorrow,
+        [Display(Name = "Xcode")]
         xcode,
+        [Display(Name = "Kuroir")]
         kuroir,
+        [Display(Name = "Katzenmilch")]
         katzenmilch,
+        [Display(Name = "SQL Server")]
         sqlserver,
+        [Display(Name = "Ambiance")]
         ambiance,
+        [Display(Name = "Chaos")]
         chaos,
+        [Display(Name = "Clouds Midnight")]
         clouds_midnight,
+        [Display(Name = "Dracula")]
         dracula,
+        [Display(Name = "Cobalt")]
         cobalt,
+        [Display(Name = "Gruvbox")]
         gruvbox,
+        [Display(Name = "GOB")]
         gob,
+        [Display(Name = "Idle Fingers")]
         idle_fingers,
+        [Display(Name = "KR AceThemeWithAttributes")]
         kr_theme,
+        [Display(Name = "Merbivore")]
         merbivore,
+        [Display(Name = "Merbivore Soft")]
         merbivore_soft,
+        [Display(Name = "Mono Industrial")]
         mono_industrial,
+        [Display(Name = "Monokai")]
         monokai,
+        [Display(Name = "Nord Dark")]
         nord_dark,
+        [Display(Name = "One Dark")]
         one_dark,
+        [Display(Name = "Pastel on Dark")]
         pastel_on_dark,
+        [Display(Name = "Solarized Dark")]
         solarized_dark,
+        [Display(Name = "Terminal")]
         terminal,
+        [Display(Name = "Tomorrow Night")]
         tomorrow_night,
+        [Display(Name = "Tomorrow Night Blue")]
         tomorrow_night_blue,
+        [Display(Name = "Tomorrow Night Bright")]
         tomorrow_night_bright,
+        [Display(Name = "Tomorrow Night Eighties")]
         tomorrow_night_eighties,
+        [Display(Name = "Twilight")]
         twilight,
+        [Display(Name = "Vibrant Ink")]
         vibrant_ink
     }
+    
+
+    public class AceThemeWithAttributes
+    {
+        public AceTheme Name { get; set; }
+        public string? DisplayName { get; set; }
+        public AceThemeType Type { get; set; }
+    }
+
+     public static class AceThemeRepository
+    {
+        public static List<AceThemeWithAttributes> GetThemes()
+        {
+            return new List<AceThemeWithAttributes>
+            {
+                new AceThemeWithAttributes { Name = AceTheme.chrome, DisplayName = "Chrome", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.clouds, DisplayName = "Clouds", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.crimson_editor, DisplayName = "Crimson Editor", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.dawn, DisplayName = "Dawn", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.dreamweaver, DisplayName = "Dreamweaver", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.eclipse, DisplayName = "Eclipse", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.github, DisplayName = "GitHub", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.iplastic, DisplayName = "iPlastic", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.solarized_light, DisplayName = "Solarized Light", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.textmate, DisplayName = "TextMate", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.tomorrow, DisplayName = "Tomorrow", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.xcode, DisplayName = "Xcode", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.kuroir, DisplayName = "Kuroir", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.katzenmilch, DisplayName = "Katzenmilch", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.sqlserver, DisplayName = "SQL Server", Type = AceThemeType.Bright },
+                new AceThemeWithAttributes { Name = AceTheme.ambiance, DisplayName = "Ambiance", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.chaos, DisplayName = "Chaos", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.clouds_midnight, DisplayName = "Clouds Midnight", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.dracula, DisplayName = "Dracula", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.cobalt, DisplayName = "Cobalt", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.gruvbox, DisplayName = "Gruvbox", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.gob, DisplayName = "GOB", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.idle_fingers, DisplayName = "Idle Fingers", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.kr_theme, DisplayName = "KR AceThemeWithAttributes", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.merbivore, DisplayName = "Merbivore", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.merbivore_soft, DisplayName = "Merbivore Soft", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.mono_industrial, DisplayName = "Mono Industrial", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.monokai, DisplayName = "Monokai", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.nord_dark, DisplayName = "Nord Dark", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.one_dark, DisplayName = "One Dark", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.pastel_on_dark, DisplayName = "Pastel on Dark", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.solarized_dark, DisplayName = "Solarized Dark", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.terminal, DisplayName = "Terminal", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.tomorrow_night, DisplayName = "Tomorrow Night", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.tomorrow_night_blue, DisplayName = "Tomorrow Night Blue", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.tomorrow_night_bright, DisplayName = "Tomorrow Night Bright", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.tomorrow_night_eighties, DisplayName = "Tomorrow Night Eighties", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.twilight, DisplayName = "Twilight", Type = AceThemeType.Dark },
+                new AceThemeWithAttributes { Name = AceTheme.vibrant_ink, DisplayName = "Vibrant Ink", Type = AceThemeType.Dark }
+            };
+        }
+    }
+
 }
